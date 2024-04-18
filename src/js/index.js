99ae0127f7833aefaf0b004185216bce897a919f
Katlego J Mogashoa <106239836+katGhost@users.noreply.github.com>
2024-04-18 09:56:27 +0200

    changes in plans-wrapper

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
 =========================================================*/
 const productsDOM = document.querySelector(".products-center");
 const bannerBtn = document.querySelector(".banner-btn");
-const plansCard = document.querySelectorAll(".plan-wrapper");
+const plansCardWrapper = document.querySelectorAll(".plan-wrapper");
 /*========================================================
    DOM END
 =========================================================*/
@@ -47,7 +47,7 @@ window.addEventListener("DOMContentLoaded", () => {
       allSlides[slideIndex - 1].style.display = "block";
   
       // Apply animation to slide from left while fading
-      allSlides[slideIndex - 1].style.animation = "slideAndFade 5s ease-out";
+      allSlides[slideIndex - 1].style.animation = "slideAndFade 7s ease-out";
     }
   
     // Start the carousel
@@ -56,9 +56,54 @@ window.addEventListener("DOMContentLoaded", () => {
   
 
 // vanilla tilt on subscription cards
-VanillaTilt.init(plansCard, {
-  max: 15,
+VanillaTilt.init(plansCardWrapper, {
+  max: 10,
   speed: 250,
   glare: true,
-  "max-glare": 1,
+  "max-glare": 0.6,
+});
+
+// ON plan card hover, mouse follow
+plansCardWrapper.forEach(planCard => {
+  planCard.addEventListener('mousemove', (e) => {
+    move(e);
+  })  
+});
+
+var mouse;
+var cursor = document.getElementById('cursor');
+
+
+//Detect touch device ======== Will work only on Android & IOS devices
+function isTouchDevice() {
+  try {
+    //Will fail for desktops and throw error
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const move = (e) => {
+  try {
+    var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+    var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+  } catch (e) {
+    console.warn('Error!');
+  }
+  //setting the left and top of div based on mouse position
+  cursor.style.left = x - 55 + "px";
+  cursor.style.top = y - 55 + "px";
+};
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) {
+    header.style.backgroundColor = "#000111";
+  } else {
+    header.style.backgroundColor = "transparent"; // Reset to default
+  }
 });
